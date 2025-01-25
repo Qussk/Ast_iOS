4d9179d7b993373c20c2fa22ced01226ebbafedb
Qussk <by1106007@gmail.com>
2025-01-25 14:54:54 +0900

    feat: add leadType

--- Ast/Ast/Sources/Application/Common/Manager/App.swift
+++ Ast/Ast/Sources/Application/Common/Manager/App.swift
@@ -12,18 +12,46 @@ import UIKit
 final class App {
     static let shared = App()
     
+    @Published private(set) var leadType: LeadType = .daily
     @Published private(set) var mainMenuToggle: Void = Void()
     @Published private(set) var goCalendarToggle: Void = Void()
 
+    private var deviceModelName: String = ""
+    
+    private init() {}
 }
 
 // MARK: - Main Menu Control
 extension App {
     func toggleMainMenu() {
+        leadType = .daily
         mainMenuToggle = Void()
     }
     
     func toReportScreen() {
         goCalendarToggle = Void()
+    }
+}
+
+extension App {
+    func getDeviceModelName() -> String {
+        
+        guard deviceModelName == "" else {
+            return deviceModelName
+        }
+        
+        if let name = ProcessInfo.processInfo.environment["SIMULATOR_DEVICE_NAME"] {
+            deviceModelName = "SIMULATOR - \(name)"
+        } else {
+            
+            let selName = "_\("deviceInfo")ForKey:"
+            let selector = NSSelectorFromString(selName)
+            
+            if UIDevice.current.responds(to: selector) {
+                deviceModelName = String(describing: UIDevice.current.perform(selector, with: "marketing-name").takeRetainedValue())
+            }
+        }
+        
+        return deviceModelName
     }
 }
